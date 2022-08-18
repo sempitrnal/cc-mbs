64cbe005d04a51f15b5757562be8c6e27e227bb5
Reynald Sampelo <reynaldsampelo@bo-mbp-69.local>
2022-08-18 15:28:00 +0800

    movie tab transition fix, tickets page and among other things

--- next.config.js
+++ next.config.js
@@ -10,14 +10,6 @@ const nextConfig = {
 			"platform-lookaside.fbsbx.com",
 		],
 	},
-	webpackDevMiddleware: (config) => {
-		// Solve compiling problem via vagrant
-		config.watchOptions = {
-			poll: 1000, // Check for changes every second
-			aggregateTimeout: 300, // delay before rebuilding
-		};
-		return config;
-	},
 };
 
 module.exports = nextConfig;
