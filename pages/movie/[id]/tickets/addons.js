02e062037ad13a33ea531fcb908a0c99e9481c13
Reynald Sampelo <reynalsampelo@gmail.com>
2022-09-09 16:31:16 +0800

    cart functionalities done

--- pages/movie/[id]/tickets/addons.js
+++ pages/movie/[id]/tickets/addons.js
@@ -122,42 +122,86 @@ export default function Seats() {
 						})}
 					</Swiper>
 				</div>
-				<div className="w-full h-[80vh] bg-neutral-800 rounded-[3rem] flex flex-col items-center relative">
+				<motion.div
+					layout
+					className="w-full h-[80vh] bg-neutral-800 rounded-[3rem] flex flex-col items-center relative"
+				>
 					<div className="absolute p-8 text-3xl bg-white border rounded-full -top-10">
 						<FiShoppingCart />
 					</div>
 					<h2 className="mb-5 text-2xl font-semibold text-center text-white uppercase mt-28">
 						Order Summary
 					</h2>
-					<div className="w-[85%] h-[60%] overflow-y-scroll cart">
+					<motion.div layout className="w-[85%] h-[60%] overflow-y-scroll cart">
 						{cart.map((e, i) => {
 							return (
-								<div className="flex items-center justify-between mb-5 text-white" key={e.id}>
+								<motion.div
+									layout
+									className="flex items-center justify-between mb-5 text-white"
+									key={e.id}
+								>
 									<div className="">
 										<p className="font-semibold">{e.item}</p>
 										<p className="mt-1 text-xs text-neutral-400">
 											Quantity <span className="font-bold text-white">{e.quantity}</span>{" "}
 										</p>
 										{i > 0 && (
-											<div className="flex">
-												<div className="">
+											<div className="flex justify-center mt-2 rounded-md w-max">
+												<div
+													onClick={() => {
+														setCart((prev) => {
+															return prev.map((p) => {
+																return p.id === e.id ? { ...p, quantity: (p.quantity += 1) } : p;
+															});
+														});
+													}}
+													className="px-1 py-0.5 text-black transition-opacity duration-100 bg-white border-r rounded-tl-sm rounded-bl-sm hover:opacity-80 "
+												>
 													<FiPlus />
 												</div>
-												<div className="">
+												<div
+													onClick={() => {
+														let newObj;
+														setCart((prev) => {
+															newObj = prev.map((p) => {
+																return p.id === e.id && p.quantity > 1
+																	? { ...p, quantity: (p.quantity -= 1) }
+																	: p.id === e.id && p.quantity === 1
+																	? {}
+																	: p;
+															});
+
+															return newObj.filter((e) => Object.keys(e).length != 0);
+														});
+														console.log(newObj);
+													}}
+													className="px-1 py-0.5 text-center text-black transition-opacity duration-100 bg-white border-r hover:opacity-80"
+												>
 													<FiMinus />
 												</div>
-												<div className="">
+												<div
+													onClick={() => {
+														let newObj;
+														setCart((prev) => {
+															newObj = prev.map((p) => {
+																return p.id === e.id ? {} : p;
+															});
+															return newObj.filter((e) => Object.keys(e).length != 0);
+														});
+													}}
+													className="px-1 py-0.5 text-red-600 transition duration-100 bg-white rounded-tr-sm rounded-br-sm hover:bg-red-600 hover:text-white "
+												>
 													<FiTrash />
 												</div>
 											</div>
 										)}
 									</div>
 									<div className="">₱ {e.quantity * e.price}</div>
-								</div>
+								</motion.div>
 							);
 						})}
-					</div>
-				</div>
+					</motion.div>
+				</motion.div>
 			</motion.div>
 		</div>
 	);
